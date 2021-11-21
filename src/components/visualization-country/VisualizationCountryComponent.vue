abcfba972ed70d531e3b2021acba2198eeaea670
jeremias ramirez <jereramirez499@gmail.com>
2021-11-21 00:26:14 -0400

    mostrando datos cuando hayan arrivados

--- src/components/visualization-country/VisualizationCountryComponent.vue
+++ src/components/visualization-country/VisualizationCountryComponent.vue
@@ -4,17 +4,24 @@
             <article  class="message bounceIn" v-if="!data">
                 <p>Elige un país para ver su información completa! </p>
             </article>
- 
-           <article class="title__visual" v-if="data">
-               <img :src="img" alt="logo country">
-               <h1>{{ data.Country }}</h1>
-               <span class="recording"></span>
-           </article>
 
-           <article class="container__info__visual" v-if="data">
-               <div class="column-1"></div>
-                <div class="column-2"></div>
-           </article>
+            <div v-if="data">
+                
+                <article class="title__visual"  >
+                    <img v-if="img" :src="img" alt="logo country">
+                    <i v-if="!img" class="fas fa-circle-notch circle animate circle-builded"></i>
+                    <h1>{{ data.Country }}</h1>
+                    <span class="recording"></span>
+                </article>
+
+                <article class="container__info__visual" v-if="data">
+                    <div class="column-1"></div>
+                    <div class="column-2"></div>
+                </article>
+
+           </div>
+
+           
         </section>
          
 </template>
